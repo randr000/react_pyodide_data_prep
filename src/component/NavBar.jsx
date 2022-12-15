851834d3aea0f925c31868686cae457b49670c91
Raul Andrial <rauljandrial@gmail.com>
2022-12-15 17:12:54 -0500

    Added navbar placeholder

---
+++ src/component/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NavBar = () => {
+
+    return (
+        <nav className="navbar bg-light">
+            <div className="container-fluid">
+                <h1 className="navbar-brand">Pyodide React App</h1>
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
