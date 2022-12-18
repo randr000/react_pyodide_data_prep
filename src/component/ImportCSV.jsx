8e8c68979240fb542ecc55b795ec35ec1cf5dd54
Raul Andrial <rauljandrial@gmail.com>
2022-12-18 15:53:48 -0500

    Created dashed border for file drop area

--- src/component/ImportCSV.jsx
+++ src/component/ImportCSV.jsx
@@ -3,12 +3,20 @@ import Draggable from 'react-draggable';
 
 const ImportCSV = () => {
 
+    const uploadStyles = {
+        border: "dashed 3px lightgray"
+    }
+
     return (
 
 
         <Draggable bounds="parent">
             <div className="card border border-primary border-3" style={{width: "12rem"}}>
                 <div className="card-body text-center">
+                    <div className="card" style={uploadStyles}>
+                        <i className="bi bi-file-earmark-arrow-up display-3"></i>
+                        <button className="btn btn-primary">Browse</button>
+                    </div>
                     <h5 className="card-title">Import CSV</h5>
                     <p className="card-text display-1" >
                         <i className="bi bi-filetype-csv"></i>
