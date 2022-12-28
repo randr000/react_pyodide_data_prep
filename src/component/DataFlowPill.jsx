9da76e9369aad804f4ab84e51aad831b875f5b00
Raul Andrial <rauljandrial@gmail.com>
2022-12-28 18:06:57 -0500

    Reformatted some of the DataFlowPill component code

--- src/component/DataFlowPill.jsx
+++ src/component/DataFlowPill.jsx
@@ -3,7 +3,18 @@ import React from 'react';
 const DataFlowPill = ({isOnTop, id}) => {
     
     return (
-        <span id={id} className={`position-absolute top-${isOnTop ? '0' : '100'} start-50 translate-middle bg-white border border-primary rounded-pill fs-5`}>
+        <span
+            id={id}
+            className={`
+                position-absolute
+                top-${isOnTop ? '0' : '100'}
+                start-50
+                translate-middle
+                bg-white border
+                border-primary
+                rounded-pill
+                fs-5
+            `}>
             <i className="bi bi-arrow-down"></i>
         </span>
   );
