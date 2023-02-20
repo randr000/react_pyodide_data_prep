f0f1bdc535f21ba14d9835966a71293d6ec91a4a
Raul Andrial <rauljandrial@gmail.com>
2023-02-20 05:08:54 -0500

    Added boilerplate Checkbox component, have the filtered data update automatically when new data is uploaded

---
+++ src/component/Checkbox.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+
+const Checkbox = () => {
+    
+    return <></>;
+}
+
+export default Checkbox;
--- src/component/Filter.jsx
+++ src/component/Filter.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext } from 'react';
 import Draggable from 'react-draggable';
 import DataFlowPill from './DataFlowPill';
 import CardSummary from './CardSummary';
+import Checkbox from './Checkbox';
 import Table from './Table';
 import filter from '../python_code_js_modules/filter';
 import { PyodideContext } from '../context/PyodideContext';
@@ -11,30 +12,24 @@ import { useXarrow } from 'react-xarrows';
 
 const Filter = ({jsonData, cardTitle, iconClassNames}) => {
 
-    const [inputData, setInputData] = useState(jsonData);
+    // const [inputData, setInputData] = useState(JSON.parse(jsonData));
     const [outputData, setOutputData] = useState(null);
     const {pyodide, isPyodideLoaded} = useContext(PyodideContext);
     const updateXarrow = useXarrow();
 
     useEffect(() => {
-        // console.log(`inputData: ${jsonData}`)
+        
         function filterDF(jsonStr) {
-            // console.log("running")
             if (isPyodideLoaded) {
                 pyodide.runPython(filter);
-                // console.log(`pyodide get ${pyodide.globals.get('filter')(jsonStr, ['city', 'population'])}`);
-                setOutputData(pyodide.globals.get('filter')(jsonStr, ['city']));
+                setOutputData(pyodide.globals.get('filter')(jsonStr, ['population']));
             }
         }
-
-        // if (inputData) filterDF(inputData);
+        
         if (jsonData) filterDF(jsonData);
         else setOutputData(null);
 
-        console.log(`jsonData: ${jsonData}`);
-        // console.log(`outputData: ${outputData}`);
-
-    }, []);
+    }, [jsonData]);
 
     return (
 
@@ -42,10 +37,11 @@ const Filter = ({jsonData, cardTitle, iconClassNames}) => {
             <div className="d-flex">
                 <div className="card border border-primary border-3" style={{width: "12rem"}}>
                     <div className="card-body text-center">
-                
+
                         <DataFlowPill isOnTop={true} id="filter" />
                         <CardSummary cardTitle={cardTitle} iconClassNames={iconClassNames} />
                         <DataFlowPill isOnTop={false} />
+
                     </div>
                 </div>
                 {outputData && <Table tableData={outputData}/>}
