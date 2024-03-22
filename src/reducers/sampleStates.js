63d3757c2516806218002b2feef944e6de2e6089
Raul Andrial <rauljandrial@gmail.com>
2024-03-21 21:36:08 -0400

    Created sample c initial state

--- src/reducers/sampleStates.js
+++ src/reducers/sampleStates.js
@@ -7,6 +7,7 @@ const sampleStates = {
         components: [],
         arrows: []
     },
+
     b: {
         nextID: 2,
         connectComponents: false,
@@ -28,6 +29,53 @@ const sampleStates = {
                 start: '0-btm',
                 end: '1-top',
                 compIDs: new Set([0, 1])
+            }
+        ]
+    },
+    
+    c: {
+        nextID: 4,
+        connectComponents: false,
+        components: [
+            {
+                type: DATA_COMPONENT_TYPES.FILE_UPLOAD,
+                compID: 0,
+                outputComponents: new Set([1, 2, 3])
+            },
+            {
+                type: DATA_COMPONENT_TYPES.FILTER_COLS,
+                compID: 1,
+                sourceComponents: new Set([0])
+            },
+            {
+                type: DATA_COMPONENT_TYPES.FILTER_COLS,
+                compID: 2,
+                sourceComponents: new Set([0])
+            },
+            {
+                type: DATA_COMPONENT_TYPES.FILTER_COLS,
+                compID: 3,
+                sourceComponents: new Set([0])
+            }
+        ],
+        arrows: [
+            {
+                arrowID: '0-btm_1-top',
+                start: '0-btm',
+                end: '1-top',
+                compIDs: new Set([0, 1])
+            },
+            {
+                arrowID: '0-btm_2-top',
+                start: '0-btm',
+                end: '2-top',
+                compIDs: new Set([0, 2])
+            },
+            {
+                arrowID: '0-btm_3-top',
+                start: '0-btm',
+                end: '3-top',
+                compIDs: new Set([0, 3])
             }
         ]
     }
