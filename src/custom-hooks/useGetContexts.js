092b464779f6bd1117654ed4fd49c1012bf1ddad
Raul Andrial <rauljandrial@gmail.com>
2024-04-18 20:03:26 -0400

    Created useGetContexts custom hook to load Pyodide and AppDataContext on one line and reduce imports

---
+++ src/custom-hooks/useGetContexts.js
@@ -0,0 +1,12 @@
+import { useState, useContext } from 'react';
+import AppDataContext from '../context/AppDataContext';
+import { PyodideContext } from '../context/PyodideContext';
+
+const useGetContexts = () => {
+
+    const {pyodide, isPyodideLoaded} = useContext(PyodideContext);
+    const {appState, dispatch} = useContext(AppDataContext);
+    return {pyodide, isPyodideLoaded, appState, dispatch};
+};
+
+export default useGetContexts;
