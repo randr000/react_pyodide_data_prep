fb1270441697a02014dce9220b2ce26712fbe2eb
Raul Andrial <rauljandrial@gmail.com>
2024-04-23 01:52:16 -0400

    created error_messages file

---
+++ src/js/error_messages.js
@@ -0,0 +1,8 @@
+const errorMessages = {
+    csvErrorMsg: 'An error occured reading the CSV file. Please make sure it is in the appropriate format.',
+    txtErrorMsg: 'An error occured reading the TXT file. Please make sure it is in the appropriate format.',
+    jsonErrorMsg: 'An error occured reading the JSON file. Please make sure it is in the appropriate format.',
+    excelErrorMsg: 'An error occured reading the Excel file. Please make sure it is in the appropriate format.'
+};
+
+export default errorMessages;
