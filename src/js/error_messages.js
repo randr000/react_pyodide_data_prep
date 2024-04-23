9483180da1a69062215a6abc7ae3eff67fb6f85f
Raul Andrial <rauljandrial@gmail.com>
2024-04-23 13:17:20 -0400

    updated variable name in error messages

--- src/js/error_messages.js
+++ src/js/error_messages.js
@@ -1,8 +1,8 @@
-const errorMessages = {
+const ERROR_MSG = {
     csvErrorMsg: 'An error occured reading the CSV file. Please make sure it is in the appropriate format.',
     txtErrorMsg: 'An error occured reading the TXT file. Please make sure it is in the appropriate format.',
     jsonErrorMsg: 'An error occured reading the JSON file. Please make sure it is in the appropriate format.',
     excelErrorMsg: 'An error occured reading the Excel file. Please make sure it is in the appropriate format.'
 };
 
-export default errorMessages;
+export default ERROR_MSG;
