4194732b61cf7d16b1385535da44b10aad82caea
Raul Andrial <rauljandrial@gmail.com>
2024-04-24 23:18:04 -0400

    Created test file for Union component

---
+++ src/components/data/Union.test.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Union from "./Union";
+
+test('true equals true', () => {
+    expect(true).toEqual(true);
+});
