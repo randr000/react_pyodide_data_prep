b90019143997002bc118d0e5cbcfb19dc8b0fbc2
Raul Andrial <rauljandrial@gmail.com>
2024-04-25 18:09:40 -0400

    made table scrollable

--- src/components/utilities/Table.jsx
+++ src/components/utilities/Table.jsx
@@ -13,28 +13,32 @@ const Table = ({tableData, show}) => {
 
     return (
           
-        <table className={`table table-striped table-hover table-bordered table-sm table-responsive w-25 ms-5 ${!show && "d-none"}` }>
-            <thead className="table-dark">
-                <tr>
-                    {columns.map(col => <th key={col} scope="column">{col}</th>)}
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    data.slice(0, 5).map((rowData, rowIdx) => {
-                        return (
-                            <tr key={`row-${rowIdx}`}>
-                                {
-                                    rowData.map((data, idx) => {
-                                        return <td key={`${columns[idx]}-${rowIdx}`}>{data}</td>
-                                    })
-                                }
-                            </tr>
-                        )
-                    })
-                }
-            </tbody>
-        </table>
+        <div title="table-container" className="table-container">
+            <table
+                className={`table table-striped table-hover table-bordered table-sm table-responsive w-25 ms-5 overflow-scroll ${!show && "d-none"}` }
+            >
+                <thead className="table-dark data-th">
+                    <tr>
+                        {columns.map(col => <th key={col} scope="column" >{col}</th>)}
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        data.map((rowData, rowIdx) => {
+                            return (
+                                <tr key={`row-${rowIdx}`} className="data-tr">
+                                    {
+                                        rowData.map((data, idx) => {
+                                            return <td key={`${columns[idx]}-${rowIdx}`}>{data}</td>
+                                        })
+                                    }
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
     );
 };
 
