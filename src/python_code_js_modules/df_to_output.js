99ec0d32802f1b51de33577fc445816a4f2eb23d
Raul Andrial <rauljandrial@gmail.com>
2024-05-01 12:07:20 -0400

    Updated df_to_output

--- src/python_code_js_modules/df_to_output.js
+++ src/python_code_js_modules/df_to_output.js
@@ -2,18 +2,16 @@ const df_to_output = `
 
 import pandas as pd
 import json
+from io import StringIO
 
 def df_to_output(json_str, file_types):
 
     data_dict = {}
 
-    df = pd.read_json(path_or_buf=json_str, orient='split')
+    df = pd.read_json(path_or_buf=StringIO(json_str), orient='split')
 
     if 'csv' in file_types or 'txt' in file_types:
-        with open('/_.csv', 'w') as f:
-            df.to_csv(f, index=False, mode='w')
-        with open('/_.csv', 'r') as f:
-            data_dict['csv_txt'] = f.read()
+        data_dict['csv_text'] = df.to_csv(index=False)
 
     if 'xlsx' in file_types or 'json (records)' in file_types:
         data_dict['xlsx_json'] = df.to_json(orient='records')
