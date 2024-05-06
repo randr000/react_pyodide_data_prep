bc0011afd44138075fc2056794d02b61de168e3f
Raul Andrial <rauljandrial@gmail.com>
2024-05-06 02:59:15 -0400

    created cypress test to test dragging of a data component

--- cypress/e2e/initial-render.cy.js
+++ cypress/e2e/initial-render.cy.js
@@ -35,13 +35,26 @@ describe('App End to End Test', () => {
     });
   });
 
-  it('test dragging', () => {
+  it('Should drag a data component', () => {
 
     cy.contains('Upload').click();
     cy.contains('h5', 'Upload')
-      .trigger('mousedown')
-      .trigger('mousemove', {clientX: 500, clientY: 500})
-      .trigger('mouseup');
+      .then(target => {
+        const coords = target[0].getBoundingClientRect();
+        
+        cy.contains('h5', 'Upload')
+          .trigger('mousedown')
+          .trigger('mousemove', {clientX: 500, clientY: 500})
+          .trigger('mouseup');
+
+        cy.contains('h5', 'Upload').then(target => {
+          const newCoords = target[0].getBoundingClientRect();
+          expect(newCoords.x).to.be.greaterThan(coords.x);
+          expect(newCoords.y).to.be.greaterThan(coords.y);
+        });
+      });
+
+      cy.get('.bi-x-circle').trigger('mouseover', {force: true}).click();
   });
 
 });
