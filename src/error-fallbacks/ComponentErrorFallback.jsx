ea6ebef4cb6c51cb285d1eca59280a64e9e21211
Raul Andrial <rauljandrial@gmail.com>
2024-05-17 03:10:48 -0400

    Made style change to ComponentErrorFallback so that the entire component is now visible with fixed navbar

--- src/error-fallbacks/ComponentErrorFallback.jsx
+++ src/error-fallbacks/ComponentErrorFallback.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ComponentErrorFallback = () => {
 
     return (
-        <div className="d-flex flex-column align-items-center mt-5">
+        <div className="d-flex flex-column justify-content-center align-items-center" style={{height: "100vh"}}>
             <p className="fs-1">There has been an error processing the data of one or more of the components. Please reload page.</p>
             <p className="fs-1">Please see the console for more information.</p>
             <button className="fs-1 btn btn-primary" onClick={() => window.location.reload()}>Reload</button>
