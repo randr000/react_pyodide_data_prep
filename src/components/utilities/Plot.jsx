dc889d2bf6e894341bb6d3a99281f3da9b3c912e
Raul Andrial <rauljandrial@gmail.com>
2024-06-01 02:02:29 -0400

    can render multiple plots but in reverse order

--- src/components/utilities/Plot.jsx
+++ src/components/utilities/Plot.jsx
@@ -6,19 +6,29 @@ const Plot = ({show, compID, plotScript}) => {
     const {pyodide, isPyodideLoaded} = useGetContexts();
 
     const plotRef = useRef(null);
-    const [run, setRun] = useState(false);
+    // const [run, setRun] = useState(false);
+    const [plots, setPlots] = useState([]);
     
     function plot() {
-        if (isPyodideLoaded) {
+        if (isPyodideLoaded && document.body.childElementCount > 2) {
             try {
-                console.log(`pyScript: ${plotScript}`);
-                pyodide.runPython(plotScript);
-                if (document.body.childElementCount > 2) {
-                    // const lastChild = document.body.lastChild;
-                    // plotRef.current.appendChild(document.body.lastChild);
-                    plotRef.current.replaceChildren(document.body.lastChild);
-                }
-                // pyodide.globals.get('draw_plot')();
+    
+                // while (plotRef.current.firstChild) {
+                //     plotRef.current.removeChild(plotRef.current.firstChild);
+                // }
+                // pyodide.runPython(plotScript);
+
+                // while (document.body.childElementCount > 4) {
+                //     const lastChild = document.body.lastElementChild;
+
+                    
+
+                //     /^matplotlib_.*/.test(lastChild.id) && plotRef.current.appendChild(lastChild);
+                //     console.log(`count ${document.body.childElementCount}`);
+
+                // }
+
+
             } catch (error) {
                 console.log(error);
             }
@@ -36,15 +46,16 @@ const Plot = ({show, compID, plotScript}) => {
         // document.pyodideMplTarget = plotRef.current;
         // document.pyodideMplTarget = document.getElementById('this-plot');
         plot();
-        console.log('this');
+
         // console.log(document.pyodideMplTarget);
-        console.log(`body len ${document.body.children.length}`)
+
         // console.log(document.thispy)
     }, [plotScript])
 
     return (
         <div title="plot-container" className="plot-container">
             <div id="this-plot" ref={plotRef} data-testid={`plot-${compID}`} style={{zIndex: 1000}}>
+                {plots.map((plot, idx) => (<div key={idx}>{plot}</div>))}
             </div>
         </div>
     );
--- test.py
+++ test.py
@@ -20,3 +20,21 @@ vmax=abs(Z).max(),
 vmin=-abs(Z).max(),
 )
 plt.show()
+
+
+# creating the dataset
+data = {'C':20, 'C++':15, 'Java':30, 
+        'Python':35}
+courses = list(data.keys())
+values = list(data.values())
+  
+fig = plt.figure(figsize = (10, 5))
+ 
+# creating the bar plot
+plt.bar(courses, values, color ='maroon', 
+        width = 0.4)
+ 
+plt.xlabel("Courses offered")
+plt.ylabel("No. of students enrolled")
+plt.title("Students enrolled in different courses")
+plt.show()
